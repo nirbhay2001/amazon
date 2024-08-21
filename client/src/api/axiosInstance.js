99db4a943a554c4e17b94df8383813d51952a005
nirbhay2001 <nirbhayggupta2001@gmail.com>
2024-08-21 14:23:54 +0530

    dffsgrtre

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://amazon-clone-h0aw.onrender.com",
+  baseURL: "https://amazon-clone-yrnp.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
